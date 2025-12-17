a02e8ca6dec9453745f0bb732af3bc310f0b606c
sixleaveakkm <teki.go@finatext.com>
2025-12-17 13:16:27 +0900

    ui change

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,23 @@ import {Codemirror} from "vue-codemirror";
 
 // @ts-ignore
 import {ansi} from "@cookshack/codemirror-ansi";
-import {oneDark} from "@codemirror/theme-one-dark";
+import {materialDark} from "@fsegurai/codemirror-theme-material-dark";
+import { EditorView } from "@codemirror/view";
+
 
 const codemirrorExtensions = [
   ansi(),
-  oneDark,
+  materialDark,
+  EditorView.lineWrapping,
 ];
 </script>
 
 <template>
-  <codemirror class="codemirror" :extensions="codemirrorExtensions" autofocus :style="{height: '100vh'}"/>
+  <codemirror class="codemirror" :extensions="codemirrorExtensions" autofocus :style="{height: '100vh', fontSize: '16px'}"/>
 </template>
 
-<style scoped>
+<style>
+.cm-specialChar {
+  display: none;
+}
 </style>
